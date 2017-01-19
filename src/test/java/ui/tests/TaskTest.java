aad7011d5a4dbc8c2831e508026cb3f729398cac
Henrique Cassús <henrique.cassus@gmail.com>
2017-01-19 23:36:35 +0100

    improving teardown logic

--- src/test/java/ui/tests/TaskTest.java
+++ src/test/java/ui/tests/TaskTest.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import taskList.TaskListApp;
+import taskList.persistence.TaskRepository;
 import ui.steps.TaskSteps;
 
 @RunWith(SpringRunner.class)
@@ -20,28 +22,32 @@ public class TaskTest {
 
     @LocalServerPort
     private String localPort;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
     private static WebDriver driver;
     private static WebDriverWait wait;
     private static TaskSteps taskSteps;
 
-    @BeforeClass
-    public static void setUpClass(){
+    @Before
+    public void setUp(){
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 5);
         taskSteps = new TaskSteps(driver, wait);
+        taskRepository.deleteAll();
+        driver.get("http://localhost:"+localPort);
     }
 
     @Test
     public void testCreateTest(){
-        driver.get("http://localhost:"+localPort);
         taskSteps
                 .createTask()
                 .checkTaskWasCreated();
     }
 
-    @AfterClass
-    public static void tearDownClass(){
-        taskSteps.deleteCreatedTask();
+    @After
+    public void tearDown(){
         driver.quit();
     }
 }
