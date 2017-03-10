fdfda1a04ece3dc9da5c3d2a737fcccfea0438e5
Henrique Cassús <henrique.cassus@friendsurance.de>
2017-03-10 16:18:04 +0100

    minor id fix for task test

--- src/test/java/tasks/ui/pages/TaskPage.java
+++ src/test/java/tasks/ui/pages/TaskPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TaskPage {
-    @FindBy(id = "tasks-inpt")
+    @FindBy(id = "task-inpt")
     private WebElement taskInput;
 
     @FindBy(id = "add-btn")
