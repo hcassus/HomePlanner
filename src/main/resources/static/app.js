f2542587d8a369374b7804f62a9253342f33de09
Henrique Cassús <henrique.cassus@gmail.com>
2017-03-11 10:52:04 +0100

    prototype for item addition page

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,29 @@
 (function () {
-  var app = angular.module('hrp', []);
+  var app = angular.module('hrp', [])
+  .filter('titleCase', toTitleCase)
+  .filter('pluralize', pluralize);
+
+  function pluralize() {
+    return function (word, count) {
+      if (count == 1 || count == -1) {
+        return word
+      }
+      return word + 's'
+    }
+
+  }
+
+  function toTitleCase() {
+    return function (str) {
+      return str
+      .toLowerCase()
+      .split(' ')
+      .map(function (word) {
+        return word[0].toUpperCase() + word.substr(1);
+      })
+      .join(' ');
+    }
+  }
 
   app.controller('TasksCtrl', ['$http', function ($http) {
 
@@ -46,5 +70,29 @@
       })
     };
 
+  }]);
+
+  app.controller('PantryCtrl', ['$http', function ($http) {
+
+    this.addItem = function () {
+      this.content.push(this.newItem);
+      this.newItem = {}
+    };
+
+    this.content = [
+      {
+        'name': 'Coca Cola 2l',
+        'quantity': 1,
+        'unit': 'BOTTLE',
+        'uuid': 'f12123-de123c-a45123-c123-d12e12'
+      },
+      {
+        'name': 'Erdinger 500ml',
+        'quantity': 10,
+        'unit': 'BOTTLE',
+        'uuid': '4dbf39-fb6745-dda544-456b-d211da'
+      }
+    ];
   }])
+
 })();
