a50293d657e756e198c885f7756d97eb72b8c940
Henrique Cassús <henrique.cassus@gmail.com>
2017-03-12 12:56:59 +0100

    Migrating tests to RestAssured
    Due to the internal type treatments of TestRestTemplate, some behaviour checks were being lost

--- src/test/java/pantry/api/PantryItemApiTest.java
+++ src/test/java/pantry/api/PantryItemApiTest.java
@@ -1,12 +1,23 @@
 package pantry.api;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.emptyIterableOf;
-import static org.hamcrest.Matchers.samePropertyValuesAs;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
+import hrp.HomePlannerApp;
+import hrp.pantry.enums.MeasurementUnits;
+import hrp.pantry.persistence.PantryItem;
+import hrp.pantry.persistence.PantryItemRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,97 +25,100 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-import java.util.UUID;
-import hrp.HomePlannerApp;
-import hrp.pantry.enums.MeasurementUnits;
-import hrp.pantry.persistence.PantryItem;
-import hrp.pantry.persistence.PantryItemRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = HomePlannerApp.class, loader = SpringBootContextLoader.class)
 public class PantryItemApiTest {
 
-  @Autowired
-  PantryItemRepository repo;
-
   @LocalServerPort
   String port;
 
-  String endpoint;
-
-  TestRestTemplate restTemplate;
-
+  @Autowired
+  PantryItemRepository repository;
 
   @Before
-  public void setUp(){
-    endpoint = "http://localhost:"+ port +"/pantry/item";
-    restTemplate = new TestRestTemplate();
-    repo.deleteAll();
+  public void setUp() {
+    RestAssured.baseURI = "http://localhost:" + port;
+    repository.deleteAll();
   }
 
   @Test
-  public void createPantryItemTest() throws Exception {
-    PantryItem item = new PantryItem(
-        "Yogurt "+ System.currentTimeMillis(),
-        null,
-        null
-    );
+  public void blackBoxItemCreationTest() throws JSONException {
+    JSONObject item = new JSONObject()
+        .put("name", "Coca Cola 2l")
+        .put("quantity", 10)
+        .put("unit", "BOTTLE");
 
-    ResponseEntity response = restTemplate.postForEntity(endpoint, item, PantryItem.class);
-    PantryItem returnedItem = (PantryItem) response.getBody();
-
-    assertThat(returnedItem, samePropertyValuesAs(item));
-    assertThat(returnedItem.getUuid(), is(notNullValue()));
+    RestAssured
+        .given()
+        .contentType(ContentType.JSON)
+        .body(item.toString())
+        .when()
+        .post("/pantry/item")
+        .then()
+        .contentType(ContentType.JSON)
+        .statusCode(200)
+        .body("name", equalTo(item.get("name")))
+        .body("quantity", equalTo(item.get("quantity")))
+        .body("unit", equalTo(item.get("unit")))
+        .body("uuid", notNullValue())
+        .body("createdAt", notNullValue())
+        .body("updatedAt", notNullValue());
   }
 
   @Test
-  public void retrieveAllCreatedItems(){
-    PantryItem item1 = new PantryItem(
-        "Coffee " + System.currentTimeMillis(),
-        1L,
-        MeasurementUnits.PACKAGE
-    );
-    PantryItem item2 = new PantryItem(
-        "Onion " + System.currentTimeMillis(),
-        3L,
-        MeasurementUnits.UNIT
-    );
+  public void retrieveAllCreatedItemsTest(){
+    PantryItem item1 = new PantryItem("Erdinger Kristall 500ml",1,MeasurementUnits.BOTTLE);
+    PantryItem item2 = new PantryItem("Erdinger Kristall 500ml",2, MeasurementUnits.BOTTLE);
+    List<PantryItem> items = Arrays.asList(item1,item2);
+    repository.save(items);
 
-    repo.save(item1);
-    repo.save(item2);
-
-    ParameterizedTypeReference<List<PantryItem>> itemListType = new ParameterizedTypeReference<List<PantryItem>>() {};
-    ResponseEntity<List<PantryItem>> response = restTemplate.exchange(endpoint, HttpMethod.GET, null, itemListType);
-    List<PantryItem> returnedItems = response.getBody();
-
-    assertThat(response.getStatusCode(), is(equalTo(HttpStatus.OK)));
-    assertThat(returnedItems.size(), is(equalTo(2)));
-    assertThat(returnedItems.get(0), samePropertyValuesAs(item1));
-    assertThat(returnedItems.get(1), samePropertyValuesAs(item2));
+    RestAssured
+        .given()
+          .contentType(ContentType.JSON)
+        .when()
+          .get("/pantry/item")
+        .then()
+          .statusCode(200)
+          .body("get(0).name", is(equalTo(item1.getName())))
+          .body("get(0).quantity", equalTo(item1.getQuantity()))
+          .body("get(0).unit", equalTo(item1.getUnit().toString()))
+          .body("get(0).uuid", notNullValue())
+          .body("get(0).createdAt", notNullValue())
+          .body("get(0).updatedAt", notNullValue())
+          .body("get(1).name", is(equalTo(item2.getName())))
+          .body("get(1).quantity", equalTo(item2.getQuantity()))
+          .body("get(1).unit", equalTo(item2.getUnit().toString()))
+          .body("get(1).uuid", notNullValue())
+          .body("get(1).createdAt", notNullValue())
+          .body("get(1).updatedAt", notNullValue());
   }
 
   @Test
-  public void deletePantryItemByUuid(){
+  public void deleteItemByUuidTest(){
     PantryItem item = new PantryItem(
         "Coca Cola 500ml"+ System.currentTimeMillis(),
-        2L,
+        2,
         MeasurementUnits.BOTTLE
     );
-    UUID uuid = repo.save(item).getUuid();
+    UUID uuid = repository.save(item).getUuid();
 
-    restTemplate.delete(endpoint+ "/" + uuid);
+    RestAssured
+        .given()
+          .contentType(ContentType.JSON)
+        .when()
+          .delete("/pantry/item/" + uuid)
+        .then()
+          .statusCode(200)
+          .body(is(""));
 
-    assertThat(repo.findAll(), emptyIterableOf(PantryItem.class));
+    assertThat(repository.findAll(), emptyIterableOf(PantryItem.class));
   }
+
+
+
 
 }
