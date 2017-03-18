0ebddf4006ad0f5e4a71eeb824c9e7e194b3dd26
Henrique Cassús <henrique.cassus@gmail.com>
2017-03-19 00:56:57 +0100

    Adding retrieve product data retrieval test

--- src/test/java/pantry/persistence/ProductGatewayTest.java
+++ src/test/java/pantry/persistence/ProductGatewayTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,39 +31,46 @@ public class ProductGatewayTest {
   @Autowired
   ProductRepository repository;
 
+  @Before
+  public void setUp() {
+    repository.deleteAll();
+  }
+
   @Test
-  public void createNewProductTest(){
+  public void createNewProductTest() {
     Product product = new Product("1234567890123", "Test Product", PackagingUnit.UNIT);
     Product persistedProduct = gateway.createOrUpdateProduct(product);
     assertThat(persistedProduct, samePropertyValuesAs(product));
   }
 
-//  @Test
-//  public void createOrIncrementProductTest(){
-//    Product product = new Product("1234567890123", "Test Product", PackagingUnit.UNIT);
-//    Product product2 = new Product("1234567890123", "Test Product", PackagingUnit.UNIT);
-//    gateway.createOrUpdateProduct(product);
-//    gateway.createOrUpdateProduct(product2);
-//
-//    List<Product> products = (List<Product>) gateway.retrieveProductsByEan("1234567890123");
-//
-//
-//    assertThat(products.size(), is(equalTo(1)));
-//    assertThat(products.get(0), samePropertyValuesAs(product));
-//  }
-
   @Test
-  public void retrieveProductsByEanTest(){
+  public void retrieveProductsByEanTest() {
     Product product = new Product("1234567890123", "Test Product", PackagingUnit.UNIT);
     Product product2 = new Product("1234567890123", "Test Product2", PackagingUnit.UNIT);
     Product product3 = new Product("3210987654321", "Test Product3", PackagingUnit.UNIT);
-    repository.save(Arrays.asList(product, product2));
+    repository.save(Arrays.asList(product, product2, product3));
 
-    List<Product> retrievedProducts = (List<Product>) gateway.retrieveProductsByEan("1234567890123");
+    List<Product> retrievedProducts = (List<Product>) gateway
+        .retrieveProductsByEan("1234567890123");
 
     assertThat(retrievedProducts.size(), is(equalTo(2)));
     assertThat(retrievedProducts.get(0), samePropertyValuesAs(product));
     assertThat(retrievedProducts.get(1), samePropertyValuesAs(product2));
   }
 
+  @Test
+  public void retrieveHighestCountProduct() {
+    Product product = new Product("1234567890123", "Coca", PackagingUnit.PACKAGE);
+    Product product2 = new Product("1234567890123", "Coca Cola 2L", PackagingUnit.BOTTLE);
+    Product product3 = new Product("1234567890123", "Cola 600ml", PackagingUnit.UNIT);
+    product.setCount(10L);
+    product2.setCount(15L);
+    product3.setCount(3L);
+
+    repository.save(Arrays.asList(product, product2, product3));
+
+    Product retrievedProduct = gateway.retrieveHighestCountProductByEanCode("1234567890123");
+
+    assertThat(retrievedProduct, samePropertyValuesAs(product2));
+  }
 }
