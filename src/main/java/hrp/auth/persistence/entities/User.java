bcce471ddfaa006737388d5d565f0289293b146a
hcassus <henriquecassus@gmail.com>
2018-05-21 22:56:07 +0200

    fixing deserialization issue

--- src/main/java/hrp/auth/persistence/entities/User.java
+++ src/main/java/hrp/auth/persistence/entities/User.java
@@ -39,4 +39,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
