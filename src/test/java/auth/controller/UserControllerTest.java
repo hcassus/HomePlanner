6be62e96498b0bd331988d40acc1ca1df6e41bec
hcassus <henriquecassus@gmail.com>
2018-05-21 22:01:27 +0200

    fixing deserialization issue

--- src/test/java/auth/controller/UserControllerTest.java
+++ src/test/java/auth/controller/UserControllerTest.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,7 +21,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -36,6 +39,9 @@ public class UserControllerTest {
     @MockBean
     CreateUserUsecase createUserUsecase;
 
+    @Captor
+    private ArgumentCaptor<User> captor;
+
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
@@ -45,7 +51,11 @@ public class UserControllerTest {
     public void createUserTest() throws Exception {
         String user = new JSONObject().put("username", "FakeUser").put("password","FakePassword").toString();
         mockMvc.perform(MockMvcRequestBuilders.post("/user").contentType(MediaType.APPLICATION_JSON).content(user));
-        verify(createUserUsecase, times(1)).execute(any(User.class));
+        verify(createUserUsecase, times(1)).execute(captor.capture());
+
+        User deserializedUser = captor.getValue();
+        assertThat(deserializedUser.getUsername(), is("FakeUser"));
+        assertThat(deserializedUser.getPassword(), is("FakePassword"));
     }
 
 }
